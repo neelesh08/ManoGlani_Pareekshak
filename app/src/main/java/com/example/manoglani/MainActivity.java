a87e252b41f13e064f601586d6b116cfb8abae12
neelesh0826 <darkcygnus082@gmail.com>
2022-07-15 20:46:05 +0530

    updated added login

--- app/src/main/java/com/example/manoglani/MainActivity.java
+++ app/src/main/java/com/example/manoglani/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.manoglani;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.example.manoglani.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.button.setVisibility(View.INVISIBLE);
+                WebSettings webSettings = binding.webView.getSettings();
+                webSettings.setJavaScriptEnabled(true);
+
+//                binding.webView.setWebViewClient(new Callback());
+                binding.webView.loadUrl("https://docs.google.com/forms/d/e/1FAIpQLSc3ol-H--KmhA17zQd5aNccH0vo9lo80L0U2suo1K6GzD2M3w/viewform");
+            }
+
+
+        });
+
+
     }
+
+
+
 }
--- app/src/main/java/com/example/manoglani/loginActivity.java
+++ app/src/main/java/com/example/manoglani/loginActivity.java
@@ -5,55 +5,58 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.manoglani.databinding.ActivityLoginBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class loginActivity extends AppCompatActivity {
-
     ActivityLoginBinding binding;
+
     FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-    binding.loginbtn.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            String Email = binding.emailEditText.getText().toString();
-            String Pass = binding.passwordEditText.getText().toString();
-            boolean checkInputs = validateInputs();
-
-            mAuth = FirebaseAuth.getInstance();
-            if(checkInputs){
-
-                mAuth.signInWithEmailAndPassword(Email , Pass ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-
-                        if(task.isSuccessful()){
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
 
-                            Toast.makeText(loginActivity.this , "Login successfull" ,Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(loginActivity.this , MainActivity.class));
-                        }
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+                    public void onClick(View view) {
+                    if(validateInputs()){
+                        mAuth = FirebaseAuth.getInstance();
+                        mAuth.signInWithEmailAndPassword(binding.emailEditText.getText().toString() , binding.passwordEditText.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(loginActivity.this, "login Successful", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(loginActivity.this , MainActivity.class));
+                                }
+                                else{
+                                    Toast.makeText(loginActivity.this, "Invalid credentials ", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(loginActivity.this, "failed", Toast.LENGTH_SHORT).show();
+                            }
+                        });
 
-                        else {
-                            Toast.makeText(loginActivity.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
-                        }
+
                     }
-                });
             }
-        }
-    });
+        });
 
     }
 
@@ -81,8 +84,6 @@ public class loginActivity extends AppCompatActivity {
 
     public boolean validatePassword(){
         String Pass = binding.passwordEditText.getText().toString();
-
-
         if(Pass.isEmpty()){
             binding.passwordEditText.setError("Password is empty");
             return false;
